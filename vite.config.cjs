815746be27bc505d804127924e480cb56d20f45f
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:23:20 +0200

    fix: fix base path

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,7 +3,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/interactive-card-details-form/",
   resolve: {
     alias: {
       '@components': resolve(__dirname, 'src/components'),
